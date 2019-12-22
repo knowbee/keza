0ffd89340e5157d7b3f21c2b4991990a4de29242
Googler <knowbeeinc@gmail.com>
2019-12-22 17:07:16 +0200

    update readme and contributors guide

--- README.md
+++ README.md
@@ -1,3 +1,54 @@
-# keza
+# Keza
 
-Generate nodejs CI template files, Eslint config files, gitignore and readme file from the command line when keza's CLI is installed globally
+[![Build Status](https://travis-ci.org/knowbee/keza.svg?branch=master)](https://travis-ci.org/knowbee/keza)
+[![npm](https://img.shields.io/npm/dt/keza.svg)](https://www.npmjs.com/package/keza)
+[![Dependency Status](https://david-dm.org/knowbee/keza.svg)](https://david-dm.org/knowbee/keza)
+[![npm](https://img.shields.io/npm/v/keza.svg)](https://www.npmjs.com/package/keza)
+[![Open Source Love](https://badges.frapsoft.com/os/v1/open-source.svg?v=102)](https://github.com/ellerbrock/open-source-badge/)
+[![Open Source Love](https://badges.frapsoft.com/os/mit/mit.svg?v=102)](https://github.com/ellerbrock/open-source-badge/)
+
+Generate nodejs CI template files, Eslint config files, gitignore, docker and readme file from the command line when keza's CLI is installed globally
+
+## Install
+
+```cli
+npm install -g keza
+```
+
+or
+
+```cli
+yarn global add keza
+```
+
+## Using Keza CLI
+
+#### Command Line
+
+```cli
+$ keza <options>
+```
+
+## Options:
+
+- `-h` or `--help`: display helper screen.
+- `-b` or `--babel`: generate .babelrc.
+- `-d` or `--docker`: generate docker file.
+- `-e` or `--eslint`: generate .eslintrc and .eslintignore file.
+- `-g` or `--gitignore`: generate nodejs .gitignore.
+- `-ho` or `--hound`: generate hound yml file.
+- `-r` or `--readme`: generate readme file.
+- `-s` or `--sequelize`: generate .sequerizerc.
+- `-t` or `--travis`: generate travis yml file.
+
+## Contribution
+
+- Please before making a PR, read first this [Contributing Guideline](./CONTRIBUTING.md)
+
+## License
+
+MIT
+
+## Author
+
+Igwaneza Bruce
---
+++ index.js
@@ -0,0 +1,90 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const clear = require("clear");
+const chalk = require("chalk");
+const figlet = require("figlet");
+const bar = require("cli-progress");
+const cli = require("commander");
+
+clear();
+const keza = new cli.Command();
+console.log(chalk.green(figlet.textSync("Keza", { horizontalLayout: "full" })));
+const progress = new bar.SingleBar({
+  format: "Done |" + chalk.magenta("{bar}") + "| {percentage}%",
+  barCompleteChar: "\u2588",
+  hideCursor: true
+});
+
+if (fs.existsSync("package.json")) {
+  helper();
+  keza.parse(process.argv);
+  process.argv.slice(2).forEach(function(item) {
+    if (item === "-b" || item === "--babel") {
+      generate(__dirname + "/templates/" + ".babelrc", ".babelrc");
+    } else if (item === "-d" || item === "--docker") {
+      generate(__dirname + "/templates/" + ".dockerignore", ".dockerignore");
+      generate(__dirname + "/templates/" + "Dockerfile", "Dockerfile");
+    } else if (item === "-e" || item === "--eslint") {
+      generate(__dirname + "/templates/" + ".eslintrc", ".eslintrc");
+      generate(__dirname + "/templates/" + ".eslintignore", ".eslintignore");
+    } else if (item === "-g" || item === "--gitignore") {
+      generate(__dirname + "/templates/" + ".gitignore", ".gitignore");
+    } else if (item === "-ho" || item === "--hound") {
+      generate(__dirname + "/templates/" + ".hound.yml", ".hound.yml");
+    } else if (item === "-r" || item === "--readme") {
+      generate(__dirname + "/templates/" + "README.md", "README.md");
+    } else if (item === "-s" || item === "--sequelizerc") {
+      generate(__dirname + "/templates/" + ".sequelizerc", ".sequelizerc");
+    } else if (item === "-t" || item === "--travis") {
+      generate(__dirname + "/templates/" + ".travis.yml", ".travis.yml");
+    }
+  });
+  if (process.argv.length == 2) {
+    fs.readdir(__dirname + "/templates", function(err, filenames) {
+      if (err) {
+        return;
+      }
+      progress.start(filenames.length);
+      filenames.forEach(file => {
+        generate(__dirname + "/templates/" + file, file);
+        progress.update(filenames.length);
+      });
+      progress.stop();
+    });
+  }
+  function generate(from, to) {
+    progress.start(to.length);
+    fs.copyFile(from, to, err => {
+      progress.update(100);
+      if (err) throw err;
+      progress.stop();
+    });
+  }
+} else {
+  helper();
+  keza.parse(process.argv);
+  console.log(
+    chalk.magenta("You have to initialize your project with npm or yarn first")
+  );
+
+  process.exit();
+}
+function helper() {
+  keza
+    .version("0.0.1")
+    .option("-b, --babel", "generate babelrc")
+    .option("-d, --docker", "generate docker file")
+    .option("-e, --eslint", "generate eslintrc and eslintignore")
+    .option("-g, --gitignore", "generate nodejs gitignore")
+    .option("-ho, --hound", "generate hound file")
+    .option("-r, --readme", "generate readme file")
+    .option("-s, --sequelize", "generate sequerizerc")
+    .option("-t, --travis", "generate travis");
+  keza.on("--help", function() {
+    console.log("Examples:");
+    console.log("  $ keza --help");
+    console.log("  $ keza -h");
+    console.log("  $ keza -b -d -t");
+  });
+}
